a3c81f7ba2aeb8b7d5b56aaacb38298d8e56f411
wwt <wengweitao@foxmail.com>
2014-05-11 21:26:17 +0800

    Push to Github

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -12,7 +13,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith('<html>'))
-        self.assertIn('<title>To-Do lists',response.content)
-        self.assertTrue(response.content.endswith('</html>'))
-
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(),expected_html)
